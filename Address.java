dc8a78381830833ef7470f89697c4d422d7eb28b
Orlando <villasano187@gmail.com>
2012-11-27 19:05:34 -0700

    the layout is done

--- Address.java
+++ Address.java
@@ -3,3 +3,103 @@
 
 public class Address
 {
+	private String street1;
+	private String street2;
+	private String city;
+	private String state;
+	private String zip;
+	private String country;
+	
+	
+	public Address(String s1, String s2, String inputCity, String inputState, String cntry, int z)
+	{
+	
+	
+	}
+	
+	public Address(String s1, String c, String st, String c, int z)
+	{
+	
+	
+	
+	}
+	
+	public Address(Address input)
+	{
+	
+	
+	}
+	
+	public String toString()
+	{
+	
+	
+	}
+	
+	public void changeStreet1(String input)
+	{
+	
+	
+	}
+	
+	public void changeStreet2(String input)
+	{
+	
+	
+	}
+	
+	public void State(String input)
+	{
+	
+	}
+	
+	public void Zip(int input)
+	{
+	
+	
+	}
+	
+	public void Country(String input)
+	{
+	
+	
+	}
+	
+	public String getStreet1()
+	{
+	
+	
+	}
+	
+	public String getStreet2()
+	{
+	
+	
+	}
+	
+	public String getCity()
+	{
+	
+	
+	}
+	
+	public String getState()
+	{
+	
+	
+	}
+	
+	public String getCountry()
+	{
+	
+	
+	}
+	
+	public int getZip()
+	{
+	
+	
+	}
+	
+}
+	
